2336e622ed5a79385f42e20f716df1a37dabd84c
suku0721 <Samiksha@0731>
2022-08-16 19:13:26 +0530

    books

--- src/main/java/com/sudhir/springproject1/controller/BookController.java
+++ src/main/java/com/sudhir/springproject1/controller/BookController.java
@@ -3,12 +3,15 @@ package com.sudhir.springproject1.controller;
 import com.sudhir.springproject1.entity.Book;
 import com.sudhir.springproject1.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController("/books")
+@RequestMapping(value = "/books" , produces = MediaType.APPLICATION_JSON_VALUE)
 public class BookController {
 
     @Autowired
--- src/main/java/com/sudhir/springproject1/controller/UserController.java
+++ src/main/java/com/sudhir/springproject1/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.sudhir.springproject1.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/users" , produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 
     @GetMapping("/")
