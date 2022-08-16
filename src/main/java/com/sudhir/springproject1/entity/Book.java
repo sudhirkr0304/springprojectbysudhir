5bf54125d81979b9090bbc15e2bd36bf507a855d
suku0721 <Samiksha@0731>
2022-08-16 19:41:26 +0530

    books

--- src/main/java/com/sudhir/springproject1/entity/Book.java
+++ src/main/java/com/sudhir/springproject1/entity/Book.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+//written by sudhir kumar
 @Entity
 @Table(name = "book")
 public class Book {
